fbd8838a103e14a531fa6ac9ae386951977220e7
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-08-12 17:48:31 +0900

    renew

--- config.scm
+++ config.scm
@@ -1,16 +1,22 @@
-(use-modules (gnu) (gnu system) (gnu services desktop)
-             (gnu packages guile) (gnu packages emacs))
+(use-modules (gnu)
+             (gnu system)
+             (gnu services desktop)
+             (gnu packages guile)
+             (gnu packages emacs))
 
 (operating-system
   (host-name "guix-box")
   (timezone "Asia/Tokyo")
   (locale "ja_JP.UTF-8")
 
+  ;; キーボード設定
+  (keyboard-layout (keyboard-layout "jp" "jp106"))
+
   ;; ブートローダー（UEFI対応）
   (bootloader
-   (grub-bootloader
-    (efi-boot-directory "/boot/efi")
-    (device #f)))
+    (bootloader grub-efi-bootloader)
+    (target "/boot/efi")
+    (keyboard-layout keyboard-layout))
 
   ;; ファイルシステム設定
   (file-systems
@@ -24,20 +30,26 @@
           (type "ext4"))))
 
   ;; ユーザーアカウント
-  (users (list (user-account
-                (name "yourusername")
-                (group "users")
-                (supplementary-groups '("wheel" "audio" "video" "network"))
-                (home-directory "/home/yourusername")
-                (shell "/bin/bash"))))
+  (users
+   (list (user-account
+          (name "yourusername")
+          (group "users")
+          (supplementary-groups '("wheel" "audio" "video" "network"))
+          (home-directory "/home/yourusername")
+          (shell (file-append bash "/bin/bash")))))
 
   ;; パッケージ
-  (packages (append (list guix guile emacs emacs-exwm git)
-                    %base-packages))
+  (packages
+   (append (list guix guile emacs emacs-exwm git)
+           %base-packages))
 
-  ;; GUI,ネットワーク (Xorg + EXWM)
-  (services (append
-             (list (service xorg-service-type)
-                   (service exwm-service-type)
-                   (service network-management-service-type))
-             %base-services)))
+  ;; GUI, ネットワーク (Xorg + EXWM)
+  (services
+   (append
+    (list
+     (service xorg-service-type
+              (xorg-configuration
+               (keyboard-layout keyboard-layout)))
+     (service exwm-service-type)
+     (service network-manager-service-type))
+    %base-services)))
