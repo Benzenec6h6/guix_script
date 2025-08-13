36307ada89f71580cedb711e2914082f7a8f265c
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-08-13 20:25:02 +0900

    40~45 use user-group

--- config.scm
+++ config.scm
@@ -1,5 +1,6 @@
 (use-modules (gnu)
              (gnu system)
+             (gnu system shadow)   ; user-group と user-account 用
              (gnu services networking)
              (gnu services xorg)
              (gnu packages bash)
@@ -10,51 +11,58 @@
              (gnu packages version-control))
 
 (operating-system
- ;; ホスト名・タイムゾーン・ロケール
- (host-name "guix-box")
- (timezone "Asia/Tokyo")
- (locale "ja_JP.UTF-8")
+  ;; ホスト名・タイムゾーン・ロケール
+  (host-name "guix-box")
+  (timezone "Asia/Tokyo")
+  (locale "ja_JP.UTF-8")
 
- ;; キーボード設定
- (keyboard-layout (keyboard-layout "jp" "jp106"))
+  ;; キーボード設定
+  (keyboard-layout (keyboard-layout "jp" "jp106"))
 
- ;; ブートローダー（UEFI対応）
- (bootloader
-  (bootloader-configuration
-   (bootloader grub-efi-bootloader)
-   (targets '("/boot/efi"))))
+  ;; ブートローダー（UEFI対応）
+  (bootloader
+    (bootloader-configuration
+      (bootloader grub-efi-bootloader)
+      (targets '("/boot/efi"))))
 
- ;; ファイルシステム設定（install.sh が置換）
- (file-systems
-  (list (file-system
-         (device "DEVICE_EFI")   ;; install.sh が置換
-         (mount-point "/boot/efi")
-         (type "vfat"))
-        (file-system
-         (device "DEVICE_ROOT")  ;; install.sh が置換
-         (mount-point "/")
-         (type "ext4"))))
+  ;; ファイルシステム設定（install.sh が置換）
+  (file-systems
+   (list (file-system
+           (device "DEVICE_EFI")   ;; install.sh が置換
+           (mount-point "/boot/efi")
+           (type "vfat"))
+         (file-system
+           (device "DEVICE_ROOT")  ;; install.sh が置換
+           (mount-point "/")
+           (type "ext4"))))
 
- ;; グループ定義（root はここで宣言するだけ）
- (groups '("root" "wheel" "audio" "video" "network" "users"))
+  ;; グループ定義（user-group 形式）
+  (groups (list (user-group (name "root") (id 0))
+                (user-group (name "wheel"))
+                (user-group (name "audio"))
+                (user-group (name "video"))
+                (user-group (name "network"))
+                (user-group (name "users"))))
 
- ;; ユーザー定義（root は書かない）
- (users (list (user-account
-               (name "teto")
-               (group "users")
-               (supplementary-groups '("wheel" "audio" "video" "network"))
-               (home-directory "/home/teto")
-               (shell (file-append bash "/bin/bash")))))
+  ;; ユーザー定義（root は %base-user-accounts に含まれるので不要）
+  (users (append
+           (list (user-account
+                   (name "teto")
+                   (group "users")
+                   (supplementary-groups '("wheel" "audio" "video" "network"))
+                   (home-directory "/home/teto")
+                   (shell (file-append bash "/bin/bash"))))
+           %base-user-accounts))   ; root などデフォルトユーザーを追加
 
- ;; パッケージ
- (packages (append (list guile-3.0 emacs emacs-exwm git)
-                   %base-packages))
+  ;; パッケージ
+  (packages (append (list guile-3.0 emacs emacs-exwm git)
+                    %base-packages))
 
- ;; サービス
- (services (append
-            (list
-             ;; Xorg
-             (service xorg-server-service-type)
-             ;; Network
-             (service network-manager-service-type))
-            %base-services)))
+  ;; サービス
+  (services (append
+              (list
+                ;; Xorg
+                (service xorg-server-service-type)
+                ;; Network
+                (service network-manager-service-type))
+              %base-services)))
