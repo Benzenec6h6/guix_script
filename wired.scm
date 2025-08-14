9fe23125982eb2dfa5cc012c3338e16b8a7a4525
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-08-15 00:20:00 +0900

    delete (load ./config.scm)

--- wired.scm
+++ wired.scm
@@ -1,11 +1,7 @@
-(use-modules (gnu services networking))
-(load "./config.scm")
-
 (define %wired-os
   (operating-system
     (inherit %common-os)
     (services (append
-               (list (service dhcp-client-service-type))
-               %base-services))))
-
-%wired-os
+               (list
+                (service network-manager-service-type))
+               (operating-system-services %common-os)))))
--- wireless.scm
+++ wireless.scm
@@ -1,16 +1,13 @@
 (use-modules (gnu services networking)
-             (gnu packages admin))  ;; wpa-supplicant パッケージ用
-(load "./config.scm")
+             (gnu packages networking))
 
 (define %wireless-os
   (operating-system
     (inherit %common-os)
-    (packages (append
-               (list wpa-supplicant)
-               (operating-system-packages %common-os)))
     (services (append
-               (list (service wpa-supplicant-service-type)
-                     (service network-manager-service-type))
-               %base-services))))
-
-%wireless-os
+               (list
+                ;; 無線LAN管理のための NetworkManager サービス
+                (service network-manager-service-type)
+                ;; WPA Supplicant サービス（必要に応じて）
+                (service wpa-supplicant-service-type))
+               (operating-system-services %common-os)))))
