3bbbbc0328f235d795081eb78ca061a5a4615beb
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-08-14 09:37:19 +0900

    add 12 packages

--- config.scm
+++ config.scm
@@ -8,62 +8,74 @@
              (gnu packages emacs)
              (gnu packages emacs-xyz)
              (gnu packages package-management)
-             (gnu packages version-control))
+             (gnu packages version-control)
+             (gnu packages networking))  ; wpa-supplicant用
+
+;; ===== 有線/無線切り替えフラグ =====
+(define use-wireless? #t) ; #t = 無線, #f = 有線
 
 (operating-system
- ;; ホスト名・タイムゾーン・ロケール
- (host-name "guix-box")
- (timezone "Asia/Tokyo")
- (locale "ja_JP.UTF-8")
+  ;; ホスト名・タイムゾーン・ロケール
+  (host-name "guix-box")
+  (timezone "Asia/Tokyo")
+  (locale "ja_JP.UTF-8")
 
- ;; キーボード設定
- (keyboard-layout (keyboard-layout "jp" "jp106"))
+  ;; キーボード設定
+  (keyboard-layout (keyboard-layout "jp" "jp106"))
 
- ;; ブートローダー（UEFI対応）
- (bootloader
-  (bootloader-configuration
-   (bootloader grub-efi-bootloader)
-   (targets '("/boot/efi"))))
+  ;; ブートローダー（UEFI対応）
+  (bootloader
+   (bootloader-configuration
+    (bootloader grub-efi-bootloader)
+    (targets '("/boot/efi"))))
 
- ;; ファイルシステム（install.sh で置換）
- (file-systems
-  (list (file-system
-         (device "DEVICE_EFI")   ;; install.sh が置換
-         (mount-point "/boot/efi")
-         (type "vfat"))
-        (file-system
-         (device "DEVICE_ROOT")  ;; install.sh が置換
-         (mount-point "/")
-         (type "ext4"))))
+  ;; ファイルシステム
+  (file-systems
+   (list (file-system
+           (device "DEVICE_EFI")
+           (mount-point "/boot/efi")
+           (type "vfat"))
+         (file-system
+           (device "DEVICE_ROOT")
+           (mount-point "/")
+           (type "ext4"))))
 
- ;; グループ設定（base のものを残す + 自分の追加）
- (groups (append
-          (list (user-group (name "teto"))) ;; 自作ユーザー用グループ
-          (list (user-group (name "network")))
-          %base-groups))
+  ;; グループ設定
+  (groups (append
+           (list (user-group (name "teto")))
+           (list (user-group (name "network")))
+           %base-groups))
 
- ;; ユーザー設定（base の root や nobody を残す）
- (users (append
-         (list (user-account
-                 (name "teto")
-                 (group "teto")
-                 (supplementary-groups '("wheel" "audio" "video" "network"))
-                 (home-directory "/home/teto")
-                 (shell (file-append bash "/bin/bash"))))
-         %base-user-accounts))
+  ;; ユーザー設定
+  (users (append
+          (list (user-account
+                  (name "teto")
+                  (group "teto")
+                  (supplementary-groups '("wheel" "audio" "video" "network"))
+                  (home-directory "/home/teto")
+                  (shell (file-append bash "/bin/bash"))))
+          %base-user-accounts))
 
- ;; パッケージ
- (packages (append (list guile-3.0 emacs emacs-exwm git wpa-supplicant)
-                   %base-packages))
+  ;; パッケージ
+  (packages (append
+             (list guile-3.0 emacs emacs-exwm git
+                   ;; 無線使用時だけ wpa-supplicant を追加
+                   (if use-wireless? wpa-supplicant '()))
+             %base-packages))
 
- ;; サービス
- (services (append
-            (list
-             ;; Xorg
-             (service xorg-server-service-type)
-             ;; Network
-             ;(service wpa-supplicant-service-type)
-             ;(service network-manager-service-type)
-             ;; 有線のみ
-             (service dhcp-client-service-type))
-            %base-services)))
+  ;; サービス
+  (services
+   (append
+    (list
+     ;; Xorg
+     (service xorg-server-service-type)
+     ;; ネットワーク
+     (if use-wireless?
+         ;; 無線モード
+         (list
+          (service wpa-supplicant-service-type)
+          (service network-manager-service-type))
+         ;; 有線モード
+         (list
+          (service dhcp-client-service-type))))
+    %base-services))))
