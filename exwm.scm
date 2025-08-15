697904c05b1dbfa15950b7814d896652af0de5da
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-08-15 18:39:40 +0900

    32~47 change exwm services

--- exwm.scm
+++ exwm.scm
@@ -2,8 +2,7 @@
              (gnu packages emacs)
              (gnu packages emacs-xyz)
              (gnu services xorg)
-             (gnu services desktop)
-             (gnu services fcitx5))
+             (gnu services desktop))
 
 (load "./config.scm")
 
@@ -30,29 +29,23 @@
                (list emacs emacs-exwm emacs-magit)
                (operating-system-packages %common-os)))
     ;; サービス
-    (services
-     (append
-      (list
-       ;; Xorg サーバーとセッション設定
-       (service xorg-server-service-type
-                (xorg-configuration
-                 (keyboard-layout (keyboard-layout "jp" "jp106"))
-                 (server-arguments '("-nolisten" "tcp"))))
-       ;; EXWM セッション
-       (service slim-service-type
-                (slim-configuration
-                 (xorg-configuration
-                  (keyboard-layout (keyboard-layout "jp" "jp106")))
-                 (session-list
-                  (list
-                   (xorg-session
+(services
+ (append
+  (list
+   (service slim-service-type
+            (slim-configuration
+             (xorg-configuration
+              (xorg-configuration
+               (keyboard-layout (keyboard-layout "jp" "jp106"))))))
+   (simple-service 'exwm-xsession
+                   xsession-service-type
+                   (xsession
                     (name "exwm")
                     (start
-                     (xorg-start-command
-                      (string-append
-                       "exec "
-                       #$(file-append emacs "/bin/emacs")
-                       " --eval '" #$emacs-exwm-init "'"))))))))
+                     #~(system* #$(file-append emacs "/bin/emacs")
+                                "--eval"
+                                #$emacs-exwm-init))))
+      
        ;; 日本語入力 fcitx5
        (service fcitx5-service-type))
       (operating-system-services %common-os)))))
