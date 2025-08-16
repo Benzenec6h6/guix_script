a6d24c140bbb392e9bd04f5e25ebd1775463af12
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-08-16 17:51:39 +0900

    24~27 add bootloader

--- config.scm
+++ config.scm
@@ -1,7 +1,10 @@
-;; 共通設定（OS、ユーザー、共通パッケージ）
+;; 共通設定（OS、ユーザー、共通パッケージ、X11）
 (use-modules (gnu)
              (gnu system)
              (gnu system shadow)
+             (gnu services xorg)
+             (gnu services desktop)
+             (gnu packages xorg)
              (gnu packages bash)
              (gnu packages fonts)
              (gnu packages guile)
@@ -13,6 +16,15 @@
     (host-name "guix-box")
     (timezone "Asia/Tokyo")
     (locale "ja_JP.UTF-8")
+
+    ;; キーボード設定
+    (keyboard-layout (keyboard-layout "jp" "jp106"))
+
+    ;; ブートローダー
+    (bootloader
+     (bootloader-configuration
+      (bootloader grub-efi-bootloader)
+      (targets '("/boot/efi"))))
 
     ;; ファイルシステム
     (file-systems
@@ -41,12 +53,13 @@
                    (shell (file-append bash "/bin/bash"))))
             %base-user-accounts))
 
-    ;; 共通パッケージ
+    ;; 共通パッケージ（X11）
     (packages (append
-               (list
-                 font-google-noto-serif-cjk
-                 font-google-noto-sans-cjk)
+               (list xorg-server xterm
+                     font-google-noto-serif-cjk font-google-noto-sans-cjk)
                %base-packages))
 
-    ;; 共通サービス
-    (services %base-services)))
+    ;; 共通サービス（X11）
+    (services (append
+               (list (service xorg-server-service-type))
+               %base-services))))
