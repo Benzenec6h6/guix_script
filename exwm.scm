d8b82d1cb1e22abf9b9c0dd91bc7287a12866c4d
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-08-16 16:27:27 +0900

    48~56 change service

--- exwm.scm
+++ exwm.scm
@@ -2,16 +2,17 @@
              (gnu packages emacs)
              (gnu packages emacs-xyz)
              (gnu services xorg)
-             (gnu services desktop))
+             (gnu services desktop)
+             (gnu services shepherd))
 
 (load "./config.scm")
 
-;; ネットワークタイプは環境変数 NETWORK に "wired" か "wireless" を指定
+;; ネットワーク設定を選択 (wired.scm / wireless.scm)
 (define network
   (or (getenv "NETWORK") "wired"))
 (load (string-append "./" network ".scm"))
 
-;; EXWM / Emacs 初期化設定
+;; Emacs/EXWM 初期化コード
 (define emacs-exwm-init
   "
 (require 'exwm)
@@ -23,15 +24,15 @@
 (scroll-bar-mode -1)
 ")
 
-;; EXWM 用 OS 定義
 (define %exwm-os
   (operating-system
     (inherit %common-os)
 
-    ;; 追加パッケージ
-    (packages (append
-               (list emacs emacs-exwm emacs-magit)
-               (operating-system-packages %common-os)))
+    ;; パッケージ
+    (packages
+     (append
+      (list emacs emacs-exwm emacs-magit)
+      (operating-system-packages %common-os)))
 
     ;; サービス
     (services
@@ -43,16 +44,22 @@
                  (xorg-configuration
                   (keyboard-layout (keyboard-layout "jp" "jp106")))))
 
-       ;; EXWM / Emacs サービス
-       (simple-service 'exwm
-                       xorg-server-service-type
-                       (start #~(list #$(file-append emacs "/bin/emacs")
-                                      "--eval"
-                                      #$emacs-exwm-init)))
+       ;; EXWM / Emacs サービス (Shepherd 管理)
+       (service shepherd-root-service-type
+                (list
+                 (shepherd-service
+                  (provision '(exwm))
+                  (requirement '(xorg-server))
+                  (start #~(make-forkexec-constructor
+                            (list #$(file-append emacs "/bin/emacs")
+                                  "--eval" #$emacs-exwm-init)))
+                  (stop #~(make-kill-destructor)))))
 
        ;; fcitx5 日本語入力
        (service fcitx5-service-type))
+
       ;; 共通サービスを継承
       (operating-system-services %common-os)))))
+
 
 %exwm-os
